6fa1c286eae625bec0b6688b6c5e7a5bf0734b77
Roman Gawenda <72347545+RGawenda@users.noreply.github.com>
2024-01-14 11:06:13 +0100

    updated requirements.txt file

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 rabbitmq_config = dict(
-    adress='localhost',
-    port='',
+    adress='rabbitmq',
+    port='5672',
     queue_input='image_to_process',
     queue_output='image_result'
 )
@@ -9,7 +9,7 @@ tensorflow_config = dict(
     class_names=['Agate', 'Amethyst', 'Azurite', 'Calcite', 'Chrysocolla', 'Citrine', 'Emerald', 'Gypsum',
                  'Labradorite', 'Malachite', 'Opal', 'Pyrite', 'Quartz', 'Ruby', 'Smoky quartz', 'Topaz', 'Tourmaline',
                  'Turquoise', 'Wulfenite'],
-    image_shape=(256, 256),
+    image_shape=(299, 299),
     num_classes=19,
-    model_path="C:\\Users\\Roman\\jup\\inz_model\\model_transfer_xception_block"
+    model_path="/app/model_ai/model"
 )
